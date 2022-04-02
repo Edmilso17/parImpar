a1bf4ab398bec01fd5fb660496224fdb23c7f2a1
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-04-01 22:17:37 -0300

    exercicio resolvido

---
+++ parImpar/Program.cs
@@ -0,0 +1,19 @@
+﻿int numeroDigitado;
+
+Console.Write("Digite um número: ");
+numeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+bool numeroEhPar = (numeroDigitado % 2) == 0;
+
+if (numeroEhPar)
+
+{
+   Console.WriteLine($"{numeroDigitado} é par");
+}
+
+else
+
+{
+   Console.WriteLine($"{numeroDigitado} é impar");
+}
+   
